45f00beb92e5afea620e11edcb3d374c533e744a
Daniel Sadamo Hirayama <daniel.sadamo@kmee.com.br>
2018-12-06 20:27:45 -0200

    Refatoracao - utiliza layers para renderizar dados
    Melhoria de desempenho

--- __init__.py
+++ __init__.py
@@ -1,28 +1,61 @@
-from flask import Flask, render_template, flash, request, url_for, redirect, session
+from flask import Flask, render_template, flash, request, url_for, redirect, \
+    session
 from wtforms import Form
-from wtforms import TextField, BooleanField,validators
+from wtforms import TextField, BooleanField, validators
 from dbconnect import connection
 import gc
+import pandas as pd
+from decimal import Decimal
 
 app = Flask(__name__)
 app.secret_key = '123'
 
+def test_char(value):
+    if type(value) is str:
+        return True
+    return False
+
+
+def df_to_geojson(df, properties, lat='latitude', lon='longitude'):
+    geojson = {'type': 'FeatureCollection', 'features': []}
+    for _, row in df.iterrows():
+        feature = {'type': 'Feature',
+                   'properties': {},
+                   'geometry': {'type': 'Point',
+                                'coordinates': []}}
+        feature['geometry']['coordinates'] = [float(row[lon]), float(row[lat])]
+        for prop in properties:
+            if prop == 'cidade':
+                feature['properties'][prop] = row[prop]
+            else:
+                feature['properties'][prop] = float(row[prop])
+        feature['properties']['icon'] = 'music'
+        geojson['features'].append(feature)
+    return geojson
+
+
 class getValor(Form):
-	valor = TextField('dado')
+    valor = TextField('dado')
+
 
 @app.route('/')
 def plotAll():
-	try:
-		mycursor,mydb = connection()
-		mycursor.execute('SELECT * from view_points WHERE UH>"25.0"')
-		data = mycursor.fetchall()
-		mycursor.close()
-		mydb.close()
-		gc.collect()
-		return render_template("map.html", getValor=data)
-	except Exception as e:
-		return(str(e))	
-		
+    try:
+        mycursor, mydb = connection()
+        mycursor.execute('SELECT * from view_projects')
+        data = mycursor.fetchall()
+        mycursor.close()
+        mydb.close()
+        gc.collect()
+        headers = ('longitude', 'latitude', 'cidade', 'unid_hab', 'invest')
+        df_data = pd.DataFrame(data=data, columns = headers)
+        cols = ['cidade', 'unid_hab', 'invest']
+        geojson = df_to_geojson(df_data, cols)
+
+        return render_template("map.html", geoJson=geojson)
+    except Exception as e:
+        return (str(e))
+
 
 if __name__ == "__main__":
-	app.run()
+    app.run()
--- static/js/custom.js
+++ static/js/custom.js
@@ -5,11 +5,12 @@
 
 mapboxgl.accessToken = 'pk.eyJ1IjoicGh4Njc4IiwiYSI6ImNqZjVpb2RieTBudnMzM2x0eXUzZHUxdnoifQ.Dev7xcoIcb2V3lIJ8FvOAw';
 map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/phx678/cjp3a11ip1ye02sn28wte5vqn',
-  center: [-55.9253,-14.235],
-  zoom: 3.5,
-  //maxBounds: bounds
+    container: 'map',
+    style: 'mapbox://styles/phx678/cjp3a11ip1ye02sn28wte5vqn?optimize=true',
+    center: [-55.9253, -14.235],
+    zoom: 3.5,
+    tolerance: 2.7
+    //maxBounds: bounds
 });
 
 
@@ -17,54 +18,88 @@ var nav = new mapboxgl.NavigationControl();
 map.addControl(nav, 'top-right');
 map.addControl(new mapboxgl.FullscreenControl());
 
+map.on('load', function () {
+
+    let mcmv_data = {
+        'type': 'geojson',
+        'data': geocode_data
+    }
+
+    map.addSource('mcmv_data', mcmv_data);
+
+    map.addLayer({
+        "id": "acima-25",
+        "type": "circle",
+        "source": "mcmv_data",
+        "paint": {
+            "circle-radius": 6,
+            "circle-color": "#B42222"
+        },
+        "filter": ["==", "$type", "Point"]
+    });
+
+        // geocode_data.features.forEach(function (marker) {
+        //
+        //     // create a HTML element for each feature
+        //     var el = document.createElement('div');
+        //     el.className = 'marker';
+        //
+        //     // make a marker for each feature and add to the map
+        //     new mapboxgl.Marker(el)
+        //         .setLngLat(marker.geometry.coordinates)
+        //         .addTo(map);
+        // });
+
+    });
+
 
 document.getElementById('visaoGeral').addEventListener('click', function () {
     map.flyTo({
-        center: [-55.9253,-14.235],
-		zoom: 3.5
+        center: [-55.9253, -14.235],
+        zoom: 3.5
     });
 });
 
 document.getElementById('sul').addEventListener('click', function () {
     map.flyTo({
-        center: [-52,-27],
-		zoom: 5.5
+        center: [-52, -27],
+        zoom: 5.5
     });
 });
 
 document.getElementById('sudeste').addEventListener('click', function () {
     map.flyTo({
-        center: [-47.2096,-20.0],
-		zoom: 5.5
+        center: [-47.2096, -20.0],
+        zoom: 5.5
     });
 });
 
 document.getElementById('nordeste').addEventListener('click', function () {
     map.flyTo({
-        center: [-45,-10],
-		zoom: 5
+        center: [-45, -10],
+        zoom: 5
     });
 });
 document.getElementById('centro-oeste').addEventListener('click', function () {
     map.flyTo({
-        center: [-56,-16],
-		zoom: 5
+        center: [-56, -16],
+        zoom: 5
     });
 });
 document.getElementById('norte').addEventListener('click', function () {
     map.flyTo({
-        center: [-60,-5],
-		zoom: 4.5 
+        center: [-60, -5],
+        zoom: 4.5
     });
 });
 
-document.getElementById('listing-group').addEventListener('change', function(e) {
-		var handler = e.target.id;
-		if (e.target.checked) {
-			map[handler].enable();
-		} else {
-			map[handler].disable();
-			
-		}
+document.getElementById('listing-group').addEventListener('change', function (e) {
+    var handler = e.target.id;
+    if (e.target.checked) {
+        map[handler].enable();
+    } else {
+        map[handler].disable();
+
+    }
 });
 
